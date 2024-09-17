296ad1224fcff2ed8eb2e7c26e53ae0205821582
Nicola <nicola.fiorella@prima.it>
2024-09-17 15:10:02 +0200

    add verify function on PkceCode

--- src/pkce.rs
+++ src/pkce.rs
@@ -2,9 +2,18 @@ use anyhow::{anyhow, Context, Result};
 use base64::engine::general_purpose::URL_SAFE_NO_PAD;
 use base64::Engine;
 use serde::Deserialize;
+use sha2::{Digest, Sha256};
 
 #[derive(Debug, Deserialize, PartialEq)]
-struct PkceCode(Vec<u8>);
+pub struct PkceCode(String);
+
+impl PkceCode {
+    pub fn verify(&self, challenge: &Self) -> bool {
+        let converted_verif = Sha256::digest(&self.0);
+        let b64_verif = URL_SAFE_NO_PAD.encode(converted_verif);
+        b64_verif == challenge.0
+    }
+}
 
 impl TryFrom<&str> for PkceCode {
     type Error = anyhow::Error;
@@ -16,10 +25,10 @@ impl TryFrom<&str> for PkceCode {
         if 128 <= value.len() {
             return Err(anyhow!("PKCE code is too long"));
         }
-        let code = URL_SAFE_NO_PAD
+        let _code = URL_SAFE_NO_PAD
             .decode(&value)
             .context("PKCE code is not base64url endcoded")?;
-        Ok(Self(code))
+        Ok(Self(value.into()))
     }
 }
 
@@ -30,20 +39,46 @@ mod tests {
     const CODE_CHALLENGE: &str = "E9Melhoa2OwvFrEMTJguCHaoeK1t8URWbuGJSstw-cM";
     const CODE_VERIFIER: &str = "dBjftJeZ4CVP-mB92K27uhbUJU1p1r_wW1gFWFOEjXk";
 
-    const RAW_CODE_CHALLENGE: [u8; 32] = [
-        19, 211, 30, 150, 26, 26, 216, 236, 47, 22, 177, 12, 76, 152, 46, 8, 118, 168, 120, 173,
-        109, 241, 68, 86, 110, 225, 137, 74, 203, 112, 249, 195,
-    ];
-
-    const RAW_CODE_VERIFIER: [u8; 32] = [
-        116, 24, 223, 180, 151, 153, 224, 37, 79, 250, 96, 125, 216, 173, 187, 186, 22, 212, 37,
-        77, 105, 214, 191, 240, 91, 88, 5, 88, 83, 132, 141, 121,
-    ];
-
     #[test]
     fn it_works() {
         let code = PkceCode::try_from(CODE_CHALLENGE);
         assert!(code.is_ok(), "Error while decoding code_challenge");
-        assert_eq!(PkceCode(RAW_CODE_CHALLENGE.into()), code.unwrap());
+        assert_eq!(PkceCode(CODE_CHALLENGE.to_string()), code.unwrap());
+    }
+
+    #[test]
+    fn short_code() {
+        let code = "abcd";
+        let code: anyhow::Result<PkceCode> = code.try_into();
+        assert!(code.is_err(), "Should be too short: {:?}", &code);
+    }
+
+    #[test]
+    fn too_long_code() {
+        let code = "a".repeat(200);
+        let code: &str = code.as_ref();
+        let result: anyhow::Result<PkceCode> = code.try_into();
+        assert!(result.is_err(), "Should be too long: {:?}", &code);
+    }
+
+    #[cfg(test)]
+    mod verification {
+        use crate::pkce::tests::CODE_CHALLENGE;
+
+        use super::{PkceCode, CODE_VERIFIER};
+
+        #[test]
+        fn good_case() {
+            let verifier_code = PkceCode(CODE_VERIFIER.into());
+            let challenge_code = PkceCode(CODE_CHALLENGE.into());
+            assert!(verifier_code.verify(&challenge_code), "verification failed",);
+        }
+
+        #[test]
+        fn bad_case() {
+            let verifier_code = PkceCode("some_random_string".into());
+            let challenge_code = PkceCode(CODE_CHALLENGE.into());
+            assert!(!verifier_code.verify(&challenge_code), "verification succeded, but should have failed",);
+        }
     }
 }
